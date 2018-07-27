7cc04b68425521f3140842d45e5b607e28371e22
Guido <guido.borrelli@mail.polimi.it>
2018-07-27 17:29:12 +0200

    Doc updated & Charts revised

--- memory.py
+++ memory.py
@@ -7,8 +7,15 @@ import alg3 as pearce
 
 GRAPH = False
 TEST_NODE_SIZE = 600
-TEST_EDGE_PROBABILITY = 600**(-1/10)
-TEST_ALG = 3
+TEST_EDGE_PROBABILITY = 0.03
+TEST_ALG = 1
+
+
+# This function handle both graph creation and algorithm test
+# Controller is given by global variable usage
+# TEST_ALG is: 1(Pearce), 2(Nuutila), 3(Tarjan)
+# GRAPH is: True(Generate and save in a file a graph), False(Function load the graph file and apply the algorithm)
+# TEST_NODE_SIZE & TEST_EDGE_PROBABILITY: parameters for graph random creation
 
 
 def memory_test():
@@ -23,15 +30,17 @@ def memory_test():
         graph = nx.read_gpickle("./graph-memory-test")
         print(nx.density(graph))
         tara = process.memory_info().rss
-        if TEST_ALG == 1:
+        if TEST_ALG == 3:
             _ = pearce.apply_alg(graph)
             memory_used = process.memory_info().rss
         elif TEST_ALG == 2:
             _ = nuutila.apply_alg(graph)
             memory_used = process.memory_info().rss
-        elif TEST_ALG == 3:
+        elif TEST_ALG == 1:
             _ = tarjan.apply_alg(graph)
             memory_used = process.memory_info().rss
+        else:
+            return -1
         increment = memory_used - tara
         percentage_increment = increment * 100 / tara
         print("Memory used: {} - Tara: {}".format(memory_used, tara))
